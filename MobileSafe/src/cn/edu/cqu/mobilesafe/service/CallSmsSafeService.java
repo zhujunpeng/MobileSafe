93e614bd0352e60edcdc75fe6b11f7fd7b798dfb
zjp <zhujunpeng90@163.com>
2015-07-24 22:34:02 +0800

    通话拦截，删除通话记录

---
+++ MobileSafe/src/cn/edu/cqu/mobilesafe/service/CallSmsSafeService.java
@@ -0,0 +1,123 @@
+package cn.edu.cqu.mobilesafe.service;
+
+import java.lang.reflect.Method;
+
+import com.android.internal.telephony.ITelephony;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import cn.edu.cqu.mobilesafe.db.dao.BlackNumberDAO;
+
+public class CallSmsSafeService extends Service {
+	public static final String TAG = "CallSmsSafeService";
+	private InnerSmsReceiver receiver;
+	private BlackNumberDAO dao;
+	private TelephonyManager tm;
+	private MyListener listener;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	private class InnerSmsReceiver extends BroadcastReceiver{
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.i(TAG,"内部广播接受者， 短信到来了");
+			//检查发件人是否是黑名单号码，设置短信拦截全部拦截。
+			Object[] objs = (Object[]) intent.getExtras().get("pdus");
+			for(Object obj:objs){
+				SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) obj);
+				//得到短信发件人
+				String sender = smsMessage.getOriginatingAddress();
+				String result = dao.findMode(sender);
+				if("2".equals(result)||"3".equals(result)){
+					Log.i(TAG,"拦截短信");
+					abortBroadcast();
+					Log.i(TAG,"拦截短信000");
+				}
+				//演示代码。
+				String body = smsMessage.getMessageBody();
+				if(body.contains("fapiao")){
+					//你的头发票亮的很  语言分词技术。
+					Log.i(TAG,"拦截发票短信");
+					abortBroadcast();
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void onCreate() {
+		dao = new BlackNumberDAO(this);
+		tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		listener = new MyListener();
+		tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+		receiver = new InnerSmsReceiver();
+		IntentFilter filter =  new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+		filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+		registerReceiver(receiver,filter);
+		super.onCreate();
+	}
+	
+	@Override
+	public void onDestroy() {
+		unregisterReceiver(receiver);
+		receiver = null;
+		
+		tm.listen(listener, PhoneStateListener.LISTEN_NONE);
+		super.onDestroy();
+	}
+	
+	private class MyListener extends PhoneStateListener{
+
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			super.onCallStateChanged(state, incomingNumber);
+			switch (state) {
+			case TelephonyManager.CALL_STATE_RINGING://零响状态。
+				String result = dao.findMode(incomingNumber);
+				if("1".equals(result)||"3".equals(result)){
+					Log.i(TAG,"挂断电话。。。。");
+					endCall();
+					deleteCallLog(incomingNumber);
+				}
+				break;
+			}
+			
+		}
+	}
+
+	public void endCall() {
+		//IBinder iBinder = ServiceManager.getService(TELEPHONY_SERVICE);
+		try {
+			//加载servicemanager的字节码
+			Class clazz = CallSmsSafeService.class.getClassLoader().loadClass("android.os.ServiceManager");
+			Method method = clazz.getDeclaredMethod("getService", String.class);
+			IBinder ibinder = (IBinder) method.invoke(null, TELEPHONY_SERVICE);
+			ITelephony.Stub.asInterface(ibinder).endCall();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 删除通话记录
+	 * @param incomingNumber
+	 */
+	public void deleteCallLog(String incomingNumber) {
+		ContentResolver resolver = getContentResolver();
+		Uri uri = Uri.parse("content://call_log/calls");
+		resolver.delete(uri, "number=?", new String[]{incomingNumber});
+	}
+}
