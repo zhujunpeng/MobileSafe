8022b1c5b3b0b83ce42f1c816c2fab72041be1b8
zjp <zhujunpeng90@163.com>
2015-07-25 11:17:30 +0800

    将SmsUtils的数据使用接口的方式暴露出来，方便短信备份的时候调用

--- MobileSafe/src/cn/edu/cqu/mobilesafe/AToolsActivity.java
+++ MobileSafe/src/cn/edu/cqu/mobilesafe/AToolsActivity.java
@@ -1,13 +1,17 @@
 package cn.edu.cqu.mobilesafe;
 
 import cn.edu.cqu.mobilesafe.utils.SmsUtils;
+import cn.edu.cqu.mobilesafe.utils.SmsUtils.BackUpCallBack;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 public class AToolsActivity extends Activity {
+	
+	private ProgressDialog pd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +30,48 @@ public class AToolsActivity extends Activity {
 	 * @param v
 	 */
 	public void smsBackup(View v){
-		try {
-			SmsUtils.backuoSms(AToolsActivity.this);
-			Toast.makeText(AToolsActivity.this, "备份成功", Toast.LENGTH_SHORT).show();
-		} catch (Exception e) {
-			Toast.makeText(AToolsActivity.this, "备份失败", Toast.LENGTH_SHORT).show();
-			e.printStackTrace();
-		}
+		pd = new ProgressDialog(this);
+		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		pd.setTitle("正在备份短信");
+		pd.show();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					SmsUtils.backuoSms(AToolsActivity.this,new BackUpCallBack() {
+						
+						@Override
+						public void onSmsBackup(int progress) {
+							pd.setProgress(progress);
+						}
+						
+						@Override
+						public void SmsMaxBackup(int max) {
+							pd.setMax(max);
+						}
+					});
+					runOnUiThread(new Runnable() {
+						public void run() {
+							Toast.makeText(AToolsActivity.this, "备份成功", Toast.LENGTH_SHORT).show();
+						}
+					});
+				} catch (Exception e) {
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							Toast.makeText(AToolsActivity.this, "备份失败", Toast.LENGTH_SHORT).show();
+						}
+					});
+					e.printStackTrace();
+				}finally{
+					pd.dismiss();
+				}
+				
+			}
+		}).start();
 	}
 	/**
 	 * 短信恢复
--- MobileSafe/src/cn/edu/cqu/mobilesafe/utils/SmsUtils.java
+++ MobileSafe/src/cn/edu/cqu/mobilesafe/utils/SmsUtils.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 
 import org.xmlpull.v1.XmlSerializer;
 
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
@@ -19,13 +20,28 @@ import android.util.Xml;
  *
  */
 public class SmsUtils {
+	
+	public interface BackUpCallBack{
+		/**
+		 * 设置更新的进度
+		 * @param progress
+		 */
+		public void onSmsBackup(int progress);
+		
+		/**
+		 * 设置更新的总进度
+		 * @param max
+		 */
+		public void SmsMaxBackup(int max);
+	}
 
 	/**
 	 * 备份用户的短信
 	 * @param context
+	 * @param progressDialog 进度条
 	 * @throws FileNotFoundException 
 	 */
-	public static void backuoSms(Context context) throws Exception{
+	public static void backuoSms(Context context,BackUpCallBack callBack) throws Exception{
 		ContentResolver resolver = context.getContentResolver();
 		File file = new File(Environment.getExternalStorageDirectory(),"backup.xml");
 		FileOutputStream fos = new FileOutputStream(file);
@@ -39,6 +55,11 @@ public class SmsUtils {
 		// 获取用户的短信
 		Uri uri = Uri.parse("content://sms/");
 		Cursor cursor = resolver.query(uri, new String[]{"body","address","type","date"}, null, null, null);
+		// 获取短信总数量
+		int max = cursor.getCount();
+//		pd.setMax(max);
+		callBack.SmsMaxBackup(max);
+		int progress = 0;
 		while (cursor.moveToNext()) {
 			String body = cursor.getString(0);
 			String address = cursor.getString(1);
@@ -62,6 +83,10 @@ public class SmsUtils {
 			serializer.endTag(null, "date");
 			
 			serializer.endTag(null, "sms");
+			// 设置备份的进度
+			progress ++;
+//			pd.setProgress(progress);
+			callBack.onSmsBackup(progress);
 		}
 		cursor.close();
 		serializer.endTag(null, "smss");
